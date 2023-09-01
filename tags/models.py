f9e06d270fe6192edb62fb893938eef8b51499c9
hkshakib <humayunkibriashakib@gmail.com>
2023-09-01 19:27:44 +0600

    feat: Create Tag models with Generic RelationShips

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    """
+    ContentType: for using Generic RelationShips
+    to create more flexible and dynamic relationships between different models
+
+    what tag applied to what Object
+
+    1) type e.g Product, Videos, Images
+    2) Id
+
+    """
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
