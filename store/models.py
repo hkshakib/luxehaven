d9694e55279d0423302ab8639065573c398d313c
hkshakib <humayunkibriashakib@gmail.com>
2023-09-01 16:12:30 +0600

    feat: Order Model Added with Choice Fields

--- store/models.py
+++ store/models.py
@@ -26,5 +26,18 @@ class Customer(models.Model):
     membership = models.CharField(max_length=1, choices=MEMBERSHIPS_CHOICES, default=MEMBERSHIP_BRONZE)
 
 
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
 
+    PAYMENT_STATUS = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
 
