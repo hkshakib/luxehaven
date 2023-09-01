903cb28887b135e300604e30fceeddf22de4eb65
hkshakib <humayunkibriashakib@gmail.com>
2023-09-01 15:52:29 +0600

    feat: Added two new apps: Store, Tags

--- luxehaven/settings.py
+++ luxehaven/settings.py
@@ -34,11 +34,14 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
     'rest_framework',
+
+    'store',
+    'tags',
 ]
 
 MIDDLEWARE = [
