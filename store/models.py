7530d4ab7f4da72cc886e70941718416322a74ca
hkshakib <humayunkibriashakib@gmail.com>
2023-09-01 16:18:28 +0600

    feat: Added oneToManyRelationships between Address model and Customer Model

--- store/models.py
+++ store/models.py
@@ -41,3 +41,8 @@ class Order(models.Model):
 
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
 
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
