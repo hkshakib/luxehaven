9c48b1b8809b02a50a08dd3c82e9a1df6610197f
hkshakib <humayunkibriashakib@gmail.com>
2023-09-02 16:40:21 +0600

    feat: Added Review Serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,7 +2,7 @@ from decimal import Decimal
 
 from rest_framework import serializers
 
-from store.models import Product, Collection
+from store.models import Product, Collection, Review
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -24,3 +24,8 @@ class ProductSerializer(serializers.ModelSerializer):
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
 
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'date', 'name', 'description', 'product']
