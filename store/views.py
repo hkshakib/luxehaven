15484e0dad9abd548d71d43c863c30355b3ff653
hkshakib <humayunkibriashakib@gmail.com>
2023-09-03 14:04:49 +0600

    feat: Cart Functionality Added with Get a cart endpoint

--- store/views.py
+++ store/views.py
@@ -1,8 +1,8 @@
 from django.db.models.aggregates import Count
 
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -54,6 +54,9 @@ class ReviewViewSet(ModelViewSet):
         return {'product_id': self.kwargs['product_pk']}
 
 
-class CartViewSet(CreateModelMixin, GenericViewSet):
-    queryset = Cart.objects.all()
+class CartViewSet(CreateModelMixin,
+                  RetrieveModelMixin,
+                  GenericViewSet):
+
+    queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
