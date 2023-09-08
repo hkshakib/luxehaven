b3c2d2b2510fa1863eff20e07cc45599d0e91806
hkshakib <humayunkibriashakib@gmail.com>
2023-09-08 16:33:38 +0600

    feat: Create Custom Permission

--- store/views.py
+++ store/views.py
@@ -1,9 +1,9 @@
 from django.db.models.aggregates import Count
 
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
